02ccc6c8941ffc203a75283c773f6ba9fdb21b60
SystemRhino <80726377+SystemRhino@users.noreply.github.com>
2023-10-20 09:33:06 -0300

    Add files via upload

--- php/script_cadastro.php
+++ php/script_cadastro.php
@@ -1,5 +1,11 @@
 <?php 
 require('conecta.php');
+
+    $ds_img = $_FILES["ds_img"];
+	$ext = substr($ds_img['name'], -4);
+    $nomeFinal = time().uniqid().$ext;
+
+if($ext == '.jpg' || $ext == 'jpeg' || $ext == '.png'){	
 	// Verificando se os inputs estão vazios
 	if ($_POST['login'] === "" or $_POST['password'] === "" or $_POST['user'] === "") {
 		echo "Preencha todos os campos!";
@@ -38,14 +44,17 @@ require('conecta.php');
 			  }elseif ($script_email->rowCount() > 0){
 			  	 echo "E-mail já cadastrado!";
 			  }else{
-				 $stmt = $conn->prepare('INSERT INTO tb_users(ds_login, ds_senha, nm_user, id_nivel) VALUES (:login, :senha, :user, :id_nivel)');
+				try{
+				if (move_uploaded_file($ds_img['tmp_name'], '../img/'.$nomeFinal)) {
+				 $stmt = $conn->prepare('INSERT INTO tb_users(ds_login, ds_senha, nm_user, ds_img, id_nivel) VALUES (:login, :senha, :user, :ds_img, :id_nivel)');
 					$stmt->execute(array(
 				 ':login' => $login,
 				 ':senha' => $senha,
 				 ':user' => $user,
+				 ':ds_img' => $nomeFinal,
 				 ':id_nivel' => 2
 				 ));
-
+				 
 				 // Consultando as informações do user
 				 $listagem = $conn->prepare("SELECT * FROM tb_users where ds_login = :login");
 				 $listagem->bindValue("login", $login);
@@ -56,9 +65,16 @@ require('conecta.php');
 				 $_SESSION['id'] = $lista['id'];
 				 $_SESSION['nivel'] = $lista['id_nivel'];
 				 echo "<meta HTTP-EQUIV='refresh' CONTENT='0'>";
-				 }
+				} 
+			}catch (PDOException $e) {
+					echo 'Error: ' . $e->getMessage();
+				}
+			}
 		}else{
 			echo 'Nome de usário não pode conter espaço!';
 		}
 	}
+}else{
+    echo 'Envie somente arquivos JPG, JPEG ou PNG!';
+}
 ?>
